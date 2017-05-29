c0e18d3439ac31b0ee9d8d9086e447b15153f030
marci <marci@DESKTOP-9DD01OM>
2017-05-29 21:37:46 +0200

    Added StringToTimeSpanConventer

--- WantedEvent/WantedEvent/MainPage.xaml.cs
+++ WantedEvent/WantedEvent/MainPage.xaml.cs
@@ -12,6 +12,37 @@ namespace WantedEvent
         public MainPage()
         {
             InitializeComponent();
+
+            StringToTimeSpanConventer stringToTimeSpanConventer = new StringToTimeSpanConventer();
+            var a = stringToTimeSpanConventer.StringToTimeSpan("18:00");
+            var b = stringToTimeSpanConventer.StringToTimeSpan("20:00");
+
+            TimeCalculate timeCalculate = new TimeCalculate();
+            var c = timeCalculate.CalculateTime(a,b);
+
+            time.Text = c.ToString();
+        }
+    }
+
+    internal class StringToTimeSpanConventer
+    {
+        public TimeSpan StringToTimeSpan(string getTime)
+        {
+            TimeSpan resultTime;
+
+            resultTime = TimeSpan.Parse(getTime);
+
+            return resultTime;
+        }
+    }
+
+    internal class TimeCalculate
+    {
+        public TimeSpan CalculateTime(TimeSpan startTime, TimeSpan endTime)
+        {
+            TimeSpan calculatedTime = endTime;
+
+            return calculatedTime.Subtract(startTime);           
         }
     }
 }
